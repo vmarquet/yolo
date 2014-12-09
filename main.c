f1b86966db7d7fb5f46cb0c46efb5c593fe471bd
vmarquet <vincent.marquet1@free.fr>
2014-12-09 17:14:17 +0100

    first commit

---
+++ main.c
@@ -0,0 +1,24 @@
+#include "stdio.h"
+#include "stdlib.h"
+
+#define bool  int
+#define true  1
+#define false 0
+
+
+int main () {
+
+
+	while(true) {
+		// afficher le prompt
+
+		// récupérer la ligne
+
+		// séparer selon les espaces / caractères spéciaux
+
+		// "exit" => break;
+		// sinon, exec et c°
+	}
+
+	return 0;
+}
