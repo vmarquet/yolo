2f11643e29f20e56dadb1f094336f1795bada5a6
Vincent Marquet <vincent.marquet1@free.fr>
2014-12-10 15:49:16 +0100

    ajout condition sur la valeur du séparateur

---
+++ README.md
@@ -0,0 +1,7 @@
+Description
+-----------
+* fait à [La RACHE](http://byatoo.com/la-rache/)
+* certifié ISO 1664
+
+
+
--- main.c
+++ main.c
@@ -6,6 +6,12 @@
 #define true  1
 #define false 0
 
+// les différents séparateurs possibles
+#define SEP_END  0  // ';'
+#define SEP_AND  1  // '&&'
+#define SEP_OR   2  // '||'
+#define SEP_NONE 3  // '&' n'impose pas de condition sur le résultat de la commande
+
 
 int main () {
 
@@ -25,24 +31,31 @@ int main () {
 		// boucle pour chaque commande dans la ligne
 		int i = 0;  // pointeur sur le caractère que l'on lit dans le buffer
 		bool line_complete = false;
+		int separator_before = SEP_NONE;
 		while(line_complete == false) {
 			// on récupère la prochaine commande
 			char* buffer2 = malloc(sizeof(char)*100);
 			int j = 0;  // pointeur sur buffer2 pour ajouter un caractère
 
 			// boucle pour chaque caractère de la commande
+			int separator_after = SEP_NONE;
 			bool command_complete = false;
 			while(j < 100 && i < 1000 && command_complete == false) {
 				switch(buffer[i]) {
 					case ';':
+					separator_after = SEP_END;
 						command_complete = true;
 						break;
 					case '&':
 						if(buffer[++i] == '&')
-							command_complete = true;
+							separator_after = SEP_AND;
+						else
+							separator_after = SEP_NONE;
+						command_complete = true;
 						break;
 					case '|':
 						if(buffer[++i] == '|')
+							separator_after = SEP_OR;
 							command_complete = true;
 						break;
 					case '\n':
@@ -58,8 +71,28 @@ int main () {
 
 				i++;
 			}
+			buffer2[j] = '\0';
 
-			printf("  %s \n", buffer2);
+			printf("  %s  ", buffer2);
+
+			switch(separator_before) {
+				case SEP_NONE:
+				case SEP_END:
+					printf("[exécuter tout le temps]");
+					break;
+				case SEP_AND:
+					printf("[n'exécuter que si la commande précédente a réussi]");
+					break;
+				case SEP_OR:
+					printf("[n'exécuter que si la commande précédente a échoué]");
+					break;
+			}
+			printf("\n");
+
+			// le séparateur après la commande que l'on exécute
+			// deviendra au prochain tour le séparateur d'avant commande
+			separator_before = separator_after;
+
 			free(buffer2);
 		}
 
