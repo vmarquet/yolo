ce62e1fa04833c0d815f00e86c00bf30ad64a78b
Vincent Marquet <vincent.marquet1@free.fr>
2014-12-19 09:23:04 +0100

    add questions 1 2 3 tp 6

---
+++ tp-6/main.c
@@ -0,0 +1,51 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+
+int sum(char);
+char translate(char);
+int dispatcher();
+
+int somme = 0;
+
+
+int main() {
+	while(1)
+		dispatcher();
+	return EXIT_SUCCESS;
+}
+
+int sum(char c) {
+	if (isdigit(c) != 0)
+		somme += c - '0';
+	else
+		somme = 0;
+	return somme;
+}
+
+char translate(char c) {
+	if (isalpha(c) == 0)
+		return c;
+	else {
+		if (isupper(c) != 0)
+			return tolower(c);
+		else
+			return toupper(c);
+	}
+}
+
+int dispatcher() {
+	char c = fgetc(stdin);
+	if (c == EOF)
+		return 0;
+	else if (isdigit(c) != 0) {
+		printf("%d", sum(c));
+		fflush(stdout);
+	}
+	else {
+		printf("%c", translate(c));
+		fflush(stdout);
+	}
+	return 1;
+}
