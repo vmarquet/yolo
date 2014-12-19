23e2a618f1387646a300bcbf2baf5421a634c346
Vincent Marquet <vincent.marquet1@free.fr>
2014-12-19 14:13:30 +0100

    add semaphore initialization code

--- tp-6/main.c
+++ tp-6/main.c
@@ -6,13 +6,17 @@
 #include <sys/types.h> 
 #include <sys/ipc.h> 
 #include <sys/shm.h>
+#include <sys/sem.h>
 
 
-int sum(char);
-char translate(char);
+int sum();
+char translate();
 int dispatcher();
 
 int somme = 0;
+int* shared_memory = NULL;
+struct sembuf operation;
+int sem_id;
 
 
 int main() {
@@ -25,20 +29,48 @@ int main() {
 	}
 
 	// we attach it (montage en mémoire)
-	int* shared_memory = (int*) shmat(segment_id, NULL, 0);
+	shared_memory = (int*) shmat(segment_id, NULL, 0);
 	if (shared_memory == (int*)0xFFFFFFFF) {
 		perror("ERROR: shmat failed ");
 		return (EXIT_FAILURE);
 	}
-	printf("shared memory attached at address %p \n", shared_memory);
+	printf("INFO: shared memory attached at address %p \n", shared_memory);
 
-	// we access it
-	shared_memory[0] = 0;
+	// to use the shared memory
+	// shared_memory[0] = ...;  int ... = shared_memory[0];
+
+	// we create the semaphore
+	sem_id = semget(IPC_PRIVATE, 1, IPC_CREAT | IPC_EXCL | 0600); // 1 semaphore, 0600 = droits
+	if (sem_id < 0) {
+		perror("ERROR: semget failed ");
+		return (EXIT_FAILURE);
+	}
+
+	// to affect a value to the semaphore
+	semctl(sem_id, 0, SETVAL, 1);
+	// arg 2: on a créé 1 sémaphore donc son numéro est 0
+	// arg 3: SETVAL <=> on veut changer une valeur, ça implique quelle sera de type int
+	// arg 4: valeur que l'on veut affecter au semaphore
+	// cf http://www.commentcamarche.net/faq/11267-utilisation-des-semaphores-systeme-v
+
+	// les opérations dans les semaphores doivent être modélisées dans une structure
+	operation.sem_num = 0;  // Numéro de notre sémaphore: le premier et le seul
+	operation.sem_op = -1;  // Pour un P() (prendre le mutex) on décrémente
+	operation.sem_flg = 0;  // paramètres optionnels, on ne s'en occupe pas
+
+	// pour prendre le mutex
+	// operation.sem_op = 1;
+	// semop(sem_id, &operation, 1);
+
+	// pour libérer le mutex
+	// operation.sem_op = -1;
+	// semop(sem_id, &operation, 1);
+
 
 	// first fork: the translate function
 	pid_t pid = fork();
 	if (pid == -1) {
-		printf("ERROR: fork failed \n");
+		perror("ERROR: fork failed ");
 		return EXIT_FAILURE;
 	}
 	else if (pid == 0) {
@@ -51,7 +83,7 @@ int main() {
 	// second fork: the sum function
 	pid = fork();
 	if (pid == -1) {
-		printf("ERROR: fork failed \n");
+		perror("ERROR: fork failed ");
 		return EXIT_FAILURE;
 	}
 	else if (pid == 0) {
@@ -66,10 +98,16 @@ int main() {
 		if(dispatcher() == 0)
 			break;
 
+	// on libère proprement le semaphore
+	semctl(sem_id, 0, IPC_RMID, 0);
+
 	return EXIT_SUCCESS;
 }
 
-int sum(char c) {
+int sum() {
+	// TODO: utiliser les semaphores
+	char c = (char)shared_memory[0];
+
 	if (isdigit(c) != 0)
 		somme += c - '0';
 	else
@@ -77,7 +115,10 @@ int sum(char c) {
 	return somme;
 }
 
-char translate(char c) {
+char translate() {
+	// TODO: utiliser les semaphores
+	char c = (char)shared_memory[0];
+
 	if (isalpha(c) == 0)
 		return c;
 	else {
@@ -90,15 +131,15 @@ char translate(char c) {
 
 int dispatcher() {
 	int c = fgetc(stdin);
+
 	if (c == EOF)
 		return 0;
-	else if (isdigit(c) != 0) {
-		printf("%d ", sum(c));
-		fflush(stdout);
-	}
-	else {
-		printf("%c", translate(c));
-		fflush(stdout);
-	}
+
+	// TODO: mettre C dans la mem partagée, utiliser un semaphore
+	// TODO: récupérer c
+
+	printf("%c ", c);
+	fflush(stdout);
+
 	return 1;
 }
