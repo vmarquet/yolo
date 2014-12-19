8d45f595af2d21e21684fdbe00b9acc564aea353
Vincent Marquet <vincent.marquet1@free.fr>
2014-12-19 20:52:30 +0100

    it works bitches (tp 6 ok)

--- tp-6/main.c
+++ tp-6/main.c
@@ -3,30 +3,40 @@
 #include <string.h>
 #include <ctype.h>  // pour isdigit(), isalpha()
 #include <unistd.h>  // pour fork()
+#include <signal.h>
 #include <sys/types.h> 
 #include <sys/ipc.h> 
 #include <sys/shm.h>
 #include <sys/sem.h>
 
+#define FLAG_USE_TRANSLATE 0
+#define FLAG_USE_SUM       1
+#define FLAG_JOB_DONE      2
 
-int sum();
-char translate();
+void sum();
+void translate();
 int dispatcher();
+void kill_childs(int);
 
-int somme = 0;
 int* shared_memory = NULL;
 struct sembuf operation;
 int sem_id;
+pid_t pid_translate;
+pid_t pid_sum;
+int somme = 0;
 
 
 int main() {
 	// we create the shared memory
-	size_t shared_segment_size = sizeof(int);
+	size_t shared_segment_size = sizeof(int)*2;
 	int segment_id = shmget(IPC_PRIVATE, shared_segment_size, IPC_CREAT | IPC_EXCL | 0600);
 	if (segment_id == -1) {
 		perror("ERROR: shmget failed ");
 		return (EXIT_FAILURE);
 	}
+	// CONVENTIONS:
+	// shared_memory[0] = le caractère à envoyer à translate() ou sum()
+	// shared_memory[1] = un flag pour savoir s'il faut appeler translate() ou sum()
 
 	// we attach it (montage en mémoire)
 	shared_memory = (int*) shmat(segment_id, NULL, 0);
@@ -66,32 +76,35 @@ int main() {
 	// operation.sem_op = -1;
 	// semop(sem_id, &operation, 1);
 
+	shared_memory[1] = FLAG_JOB_DONE;  // truc tordu
 
 	// first fork: the translate function
-	pid_t pid = fork();
-	if (pid == -1) {
+	pid_translate = fork();
+	if (pid_translate == -1) {
 		perror("ERROR: fork failed ");
 		return EXIT_FAILURE;
 	}
-	else if (pid == 0) {
+	else if (pid_translate == 0) {
 		// we are in the child process
-		// while(1)
-		// 	translate(c);
+		translate();
 		return EXIT_SUCCESS;
 	}
 
 	// second fork: the sum function
-	pid = fork();
-	if (pid == -1) {
+	pid_sum = fork();
+	if (pid_sum == -1) {
 		perror("ERROR: fork failed ");
 		return EXIT_FAILURE;
 	}
-	else if (pid == 0) {
+	else if (pid_sum == 0) {
 		// we are in the child process
-		// while(1)
-		// 	sum(c);
+		sum();
 		return EXIT_SUCCESS;
 	}
+
+	// we override the SIGINT signal (CTRL+C) to kill child processes before exiting
+	signal(SIGINT, kill_childs);
+
 
 	// program's main loop
 	while(1)
@@ -101,32 +114,74 @@ int main() {
 	// on libère proprement le semaphore
 	semctl(sem_id, 0, IPC_RMID, 0);
 
+	// we kill the two subprocesses
+	kill(pid_translate, SIGTERM);
+	kill(pid_sum, SIGTERM);
+
 	return EXIT_SUCCESS;
 }
 
-int sum() {
-	// TODO: utiliser les semaphores
-	char c = (char)shared_memory[0];
-
-	if (isdigit(c) != 0)
-		somme += c - '0';
-	else
-		somme = 0;
-	return somme;
+void kill_childs(int sig) {
+	kill(pid_translate, SIGTERM);
+	kill(pid_sum, SIGTERM);
+	// now we kill the parent process
+	signal(sig, SIG_DFL);  // we handle the signal with default handler
+	kill(getpid(), SIGINT);  // we send the signal to default handler
+	return;
 }
 
-char translate() {
-	// TODO: utiliser les semaphores
-	char c = (char)shared_memory[0];
+void sum() {
+	while(1) {
+		// on prend le mutex
+		operation.sem_op = 1;
+		semop(sem_id, &operation, 1);
 
-	if (isalpha(c) == 0)
-		return c;
-	else {
-		if (isupper(c) != 0)
-			return tolower(c);
-		else
-			return toupper(c);
+		if (shared_memory[1] == FLAG_USE_SUM) {
+			char c = (char)shared_memory[0];
+
+			if (isdigit(c) != 0)
+				somme += c - '0';
+			else
+				somme = 0;
+			shared_memory[0] = somme;
+
+			shared_memory[1] = FLAG_JOB_DONE;
+		}
+
+		// on libère le mutex
+		operation.sem_op = -1;
+		semop(sem_id, &operation, 1);
 	}
+
+	return;
+}
+
+void translate() {
+	while(1) {
+		// on prend le mutex
+		operation.sem_op = 1;
+		semop(sem_id, &operation, 1);
+
+		if (shared_memory[1] == FLAG_USE_TRANSLATE) {
+			char c = (char)shared_memory[0];
+
+			if (isalpha(c) == 0)
+				shared_memory[0] = c;
+			else {
+				if (isupper(c) != 0)
+					shared_memory[0] = tolower(c);
+				else
+					shared_memory[0] = toupper(c);
+			}
+
+			shared_memory[1] = FLAG_JOB_DONE;
+		}
+
+		// on libère le mutex
+		operation.sem_op = -1;
+		semop(sem_id, &operation, 1);
+	}
+	return;
 }
 
 int dispatcher() {
@@ -135,10 +190,47 @@ int dispatcher() {
 	if (c == EOF)
 		return 0;
 
-	// TODO: mettre C dans la mem partagée, utiliser un semaphore
-	// TODO: récupérer c
+	// on met c dans la mémoire partagée
+	shared_memory[0] = c;
 
-	printf("%c ", c);
+	// on met le bon flag pour savoir quelle fonction doit être appelée
+	int task;
+	if (isdigit(c) != 0) {
+		shared_memory[1] = FLAG_USE_SUM;
+		task = FLAG_USE_SUM;
+	}
+	else {
+		shared_memory[1] = FLAG_USE_TRANSLATE;
+		task = FLAG_USE_TRANSLATE;
+	}
+
+	// on libère le mutex
+	operation.sem_op = -1;
+	semop(sem_id, &operation, 1);
+
+	// tant que le job n'est pas fait, on laisse le mutex libre
+	while(1) {
+		// on reprend le mutex
+		operation.sem_op = 1;
+		semop(sem_id, &operation, 1);
+
+		if (shared_memory[1] != FLAG_JOB_DONE) {
+			// on libère le mutex
+			operation.sem_op = -1;
+			semop(sem_id, &operation, 1);
+		}
+		else
+			break;
+	}
+
+	// on récupère la nouvelle valeur du caractère
+	c = shared_memory[0];
+
+	if (task == FLAG_USE_TRANSLATE)
+		printf("%c ", c);
+	else if (task == FLAG_USE_SUM)
+		printf("%d ", c);
+
 	fflush(stdout);
 
 	return 1;
